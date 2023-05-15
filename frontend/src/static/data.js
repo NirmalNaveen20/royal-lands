f6702375373bccffdbe0902713ed90dc70479cae
NirmalNaveen20 <nirmalnaveen95@gmail.com>
2023-05-16 01:14:49 +0530

    Update home page

--- frontend/src/static/data.js
+++ frontend/src/static/data.js
@@ -25,59 +25,9 @@ export const navItems = [
   // branding data
   export const brandingData = [
     {
-      id: 1,
-      title: "Free Shipping",
-      Description: "From all orders over 100$",
-      icon: (
-        <svg
-          width="36"
-          height="36"
-          viewBox="0 0 36 36"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1 1H5.63636V24.1818H35"
-            stroke="#FFBB38"
-            stroke-width="2"
-            stroke-miterlimit="10"
-            stroke-linecap="square"
-          ></path>
-          <path
-            d="M8.72763 35.0002C10.4347 35.0002 11.8185 33.6163 11.8185 31.9093C11.8185 30.2022 10.4347 28.8184 8.72763 28.8184C7.02057 28.8184 5.63672 30.2022 5.63672 31.9093C5.63672 33.6163 7.02057 35.0002 8.72763 35.0002Z"
-            stroke="#FFBB38"
-            stroke-width="2"
-            stroke-miterlimit="10"
-            stroke-linecap="square"
-          ></path>
-          <path
-            d="M31.9073 35.0002C33.6144 35.0002 34.9982 33.6163 34.9982 31.9093C34.9982 30.2022 33.6144 28.8184 31.9073 28.8184C30.2003 28.8184 28.8164 30.2022 28.8164 31.9093C28.8164 33.6163 30.2003 35.0002 31.9073 35.0002Z"
-            stroke="#FFBB38"
-            stroke-width="2"
-            stroke-miterlimit="10"
-            stroke-linecap="square"
-          ></path>
-          <path
-            d="M34.9982 1H11.8164V18H34.9982V1Z"
-            stroke="#FFBB38"
-            stroke-width="2"
-            stroke-miterlimit="10"
-            stroke-linecap="square"
-          ></path>
-          <path
-            d="M11.8164 7.18164H34.9982"
-            stroke="#FFBB38"
-            stroke-width="2"
-            stroke-miterlimit="10"
-            stroke-linecap="square"
-          ></path>
-        </svg>
-      ),
-    },
-    {
       id: 2,
-      title: "Daily Surprise Offers",
-      Description: "Save up to 25% off",
+      title: "Daily Lands Updates",
+      Description: "24/7 Customer Service",
       icon: (
         <svg
           width="32"
@@ -104,8 +54,8 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Affortable Prices",
-      Description: "Get Factory direct price",
+      title: "Trusted Agents",
+      Description: "Get contact to booking",
       icon: (
         <svg
           width="32"
@@ -151,8 +101,8 @@ export const navItems = [
     },
     {
       id: 5,
-      title: "Secure Payments",
-      Description: "100% protected payments",
+      title: "100% Satisfaction",
+      Description: "Fast response",
       icon: (
         <svg
           width="32"
