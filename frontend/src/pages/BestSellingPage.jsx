7ecd7a28beeeb5b2a09f640970582a9357002a24
NirmalNaveen20 <nirmalnaveen95@gmail.com>
2023-05-15 11:00:08 +0530

    Initial commit

---
+++ frontend/src/pages/BestSellingPage.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import Header from "../components/Layout/Header";
+import Loader from "../components/Layout/Loader";
+import ProductCard from "../components/Route/ProductCard/ProductCard";
+import styles from "../styles/styles";
+import Footer from "../components/Layout/Footer";
+
+const BestSellingPage = () => {
+  const [data, setData] = useState([]);
+  const {allProducts,isLoading} = useSelector((state) => state.products);
+
+  useEffect(() => {
+    const allProductsData = allProducts ? [...allProducts] : [];
+    const sortedData = allProductsData?.sort((a,b) => b.sold_out - a.sold_out); 
+    setData(sortedData);
+  }, [allProducts]);
+
+  return (
+   <>
+   {
+    isLoading ? (
+      <Loader />
+    ) : (
+      <div>
+      <Header activeHeading={2} />
+      <br />
+      <br />
+      <div className={`${styles.section}`}>
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+          {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+        </div>
+      </div>
+      <Footer />
+    </div>
+    )
+   }
+   </>
+  );
+};
+
+export default BestSellingPage;
